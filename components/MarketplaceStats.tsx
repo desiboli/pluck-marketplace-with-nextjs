9f6bf659743670166027d53b101dfc909266517f
Mustafa Adam Alshammaa <adam.benals@gmail.com>
2022-03-17 13:59:05 +0100

    better marketplace stats ui

--- components/MarketplaceStats.tsx
+++ components/MarketplaceStats.tsx
@@ -5,20 +5,20 @@ type Props = {}
 const MarketplaceStats = (props: Props) => {
   return (
     <div className="mb-20 flex space-x-2 text-center">
-      <div className="left rounded-xl border-2 border-solid border-[#3A3847] py-5 px-14">
-        <p className="text-2xl font-bold">128k</p>
+      <div className="left rounded-xl border-2 border-solid border-[#3A3847] py-3 px-8 lg:py-5 lg:px-14">
+        <p className="font-bold md:text-2xl">128k</p>
         <span className="text-xs font-bold uppercase text-[#6c697a]">
           Gamers
         </span>
       </div>
-      <div className="center rounded-xl border-2 border-solid border-[#3A3847] py-5 px-14">
-        <p className="text-2xl font-bold">896k</p>
+      <div className="center rounded-xl border-2 border-solid border-[#3A3847] py-3 px-8 lg:py-5 lg:px-14">
+        <p className="font-bold md:text-2xl">896k</p>
         <span className="text-xs font-bold uppercase text-[#6c697a]">
           Cards
         </span>
       </div>
-      <div className="right rounded-xl border-2 border-solid border-[#3A3847] py-5 px-14">
-        <p className="text-2xl font-bold">84k</p>
+      <div className="right rounded-xl border-2 border-solid border-[#3A3847] py-3 px-8 lg:py-5 lg:px-14">
+        <p className="font-bold md:text-2xl">84k</p>
         <span className="text-xs font-bold uppercase text-[#6c697a]">
           Owners
         </span>
